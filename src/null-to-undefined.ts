c5e75f5c7c9b0db6c6b71fc335e1920f0b30c8a7
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-12-27 02:52:04 +0900

    諸々調整

--- src/null-to-undefined.ts
+++ src/null-to-undefined.ts
@@ -1,32 +1,80 @@
-// nullを全てundefinedに変換
-const nullToUndefined = <T>(object: T): T => {
-	for (const value in object) {
-		// nullの場合はundefinedにする
-		if (object[value] === null) {
-			object[value] = undefined as Extract<keyof T, undefined>;
+type Builtin =
+	| Function
+	| Date
+	| RegExp
+	| Map<any, any>
+	| Set<any>
+	| WeakMap<any, any>
+	| WeakSet<any>
+	| Error
+	| Promise<any>
+	| ArrayBuffer
+	| ArrayBufferView;
 
-			continue;
-		}
+type NullToUndefined<T> = T extends null
+	? undefined
+	: T extends Builtin
+		? T
+		: T extends (infer U)[]
+			? NullToUndefined<U>[]
+			: T extends object
+				? { [K in keyof T]: NullToUndefined<T[K]> }
+				: T;
 
-		// 配列の場合はfor文で再起的に回す
-		if (Array.isArray(object[value])) {
-			const array = object[value] as Extract<keyof T, unknown[]>;
-			for (let i = 0; i < array.length; i++) {
-				array[i] = nullToUndefined(array[i]);
-			}
-
-			continue;
-		}
-
-		// オブジェクトの場合も再起的に実行
-		if (typeof object[value] === "object") {
-			object[value] = nullToUndefined(object[value]);
-
-			continue;
-		}
+const transform = (value: any, seen: WeakMap<object, any>): any => {
+	if (value === null) {
+		return undefined;
 	}
 
-	return object;
+	if (value === undefined) {
+		return value;
+	}
+
+	if (typeof value !== "object") {
+		return value;
+	}
+
+	if (seen.has(value)) {
+		return seen.get(value);
+	}
+
+	if (
+		value instanceof Date ||
+		value instanceof RegExp ||
+		value instanceof Map ||
+		value instanceof Set ||
+		value instanceof WeakMap ||
+		value instanceof WeakSet ||
+		value instanceof Error ||
+		value instanceof Promise ||
+		ArrayBuffer.isView(value) ||
+		value instanceof ArrayBuffer
+	) {
+		return value;
+	}
+
+	if (Array.isArray(value)) {
+		const result: any[] = [];
+		seen.set(value, result);
+
+		for (const item of value) {
+			result.push(transform(item, seen));
+		}
+		return result;
+	}
+
+	const result: any = {};
+	seen.set(value, result);
+
+	for (const key of Object.keys(value)) {
+		result[key] = transform(value[key], seen);
+	}
+
+	return result;
+};
+
+const nullToUndefined = <T>(value: T): NullToUndefined<T> => {
+	return transform(value, new WeakMap()) as NullToUndefined<T>;
 };
 
 export default nullToUndefined;
